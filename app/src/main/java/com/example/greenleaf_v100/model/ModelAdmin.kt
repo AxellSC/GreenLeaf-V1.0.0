529a79771ace62631541bca8353ac860d4763ead
AxellSC <santiagoaxel488@gmail.com>
2025-05-08 23:10:50 -0600

    Login y registro agregados

--- app/src/main/java/com/example/greenleaf_v100/model/ModelAdmin.kt
+++ app/src/main/java/com/example/greenleaf_v100/model/ModelAdmin.kt
@@ -1,6 +1,14 @@
 package com.example.greenleaf_v100.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class ModelAdmin(
-    val correo:String,
-    val password:String
-)
+    val nombre: String = "",
+    val paterno: String = "",
+    val materno: String = "",
+    val email: String = "",
+    val password: String = "",
+    val tipoUsuario : String = "cliente"
+) : Parcelable
